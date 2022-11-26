4c598f87be162902f8776d7ab5bd5c02252e0346
hawnbin <dngh0825@naver.com>
2022-11-27 01:54:37 +0900

    Add get Detail

--- board/src/views/HomeView.vue
+++ board/src/views/HomeView.vue
@@ -2,7 +2,7 @@
 <el-row>
   <el-col :span="2"> </el-col>
     <el-col :span="20">
-        <el-table :data="articles" style="width: 100%">
+        <el-table @row-click="rowClicked" :data="articles" style="width: 100%">
             <el-table-column prop="id" label="id" width="80">
             
             </el-table-column>
@@ -27,23 +27,17 @@ export default {
     }
   },
   mounted() {
-    // apiBoard.getArticle(1)
-    // .then((res) => {
-    //   console.log("getArticle" , res);
-    // })
-    // .catch((e) => console.log(e));
     apiBoard.getArticles(0)
       .then((res) => {
         console.log("getArticles", res);
         this.articles = res.data;
       })
       .catch((e) => console.log(e));
-
-    // apiBoard.postArticle("userId", "title", "body")
-    //   .then((res) => {
-    //     console.log("postArticle", res);
-    //   })
-    //   .catch((e) => console.log(e));
+  },
+  methods: {
+    rowClicked(row){
+      this.$router.push({path:`/board/detail/${row.id}`})
+    }
   },
 }
 </script>
