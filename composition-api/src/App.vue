52d481dc168c5939004fab26e4f72a2c1d657ace
hawnbin <dngh0825@naver.com>
2022-11-26 18:35:41 +0900

    Add setting , reactive , ref , toRefs

---
+++ composition-api/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h2>Hello Composition API</h2>
+  <TestComponent />
+</div>
+</template>
+
+<script>
+import TestComponent from './components/composition/TestComponent.vue';
+
+export default {
+  name: 'App',
+  components: {TestComponent},
+  data(){
+    return {};
+  },
+};
+</script>
+
+<style></style>
+
---
+++ composition-api/src/components/composition/TestComponent.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <h2>{{ username }}</h2>
+  <button @click="changeName">ChangeUserName</button>
+  <h2>제품명: {{ name }}, 가격 {{ price }}</h2>
+  <button @click="changeNameProduct">제품 바꾸기</button>
+</div>
+</template>
+
+<script>
+
+import { reactive, ref, toRefs } from 'vue';
+
+export default {
+  name: 'TestComponent',
+  setup(){
+    const username = ref("hawnbin");
+    function changeName(){
+       username.value = "Messi"
+    }
+
+    const state = reactive({
+      nmae:"TV",
+      price:100,
+    })
+
+    function changeNameProduct(){
+      state.name = "세탁기",
+      state.price = 5000;
+    }
+
+    return {
+      username,
+      changeName,
+      ...toRefs(state),
+      changeNameProduct
+    };
+  },
+};
+</script>
+
+<style></style>
